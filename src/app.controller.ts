6ac2585eda4f1d4a6e3d248154a9bb0c25175ebe
JhonyGms <jhonatan.garcia.ayala.99@gmail.com>
2021-09-20 21:41:57 -0500

    config and example 1,2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('nuevo')
+  newEndponit() {
+    return 'soy nuevo';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return 'hello /sas/';
+  }
+
+  @Get('products/:id')
+  getProduct(@Param() params: any) {
+    return `product ${params.id}`;
   }
 }
