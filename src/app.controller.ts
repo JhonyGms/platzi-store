e8682ffd8a6635458c2d532dd53b093ea0471a08
JhonyGms <jhonatan.garcia.ayala.99@gmail.com>
2021-09-26 09:40:57 -0500

    definicion request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 @Controller()
 export class AppController {
@@ -7,42 +7,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('nuevo')
-  newEndponit() {
-    return 'soy nuevo';
-  }
-
-  @Get('/ruta/')
-  hello() {
-    return 'hello /sas/';
-  }
-
-  @Get('products/filter')
-  getFilter() {
-    return `yo soy una ruta`;
-  }
-
-  @Get('products/:productId')
-  getProduct(@Param('productId') productId: string) {
-    return `product ${productId}`;
-  }
-
-  @Get('categorys/:categoryId/products/:productId')
-  getCategory(
-    @Param('productId') productId: string,
-    @Param('categoryId') categoryId: string,
-  ) {
-    return `Category ${categoryId} product ${productId}`;
-  }
-
-  @Get('products')
-  getProducts(
-    @Query('limit') limit: 100,
-    @Query('offset') offset: 50,
-    @Query('brand') brand: string,
-  ) {
-    return `products limit=>${limit} offset=>${offset} brand=>${brand}`;
   }
 }
---
+++ src/controllers/products/products.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpStatus,
+  HttpCode,
+  Res,
+} from '@nestjs/common';
+
+import { response, Response } from 'express';
+
+@Controller('products')
+export class ProductsController {
+  @Get('filter')
+  getFilter() {
+    return { message: `yo soy una ruta` };
+  }
+
+  @Get(':productId')
+  getProduct(@Param('productId') productId: string) {
+    return { message: `product ${productId}` };
+  }
+
+  @Get('')
+  getProducts(
+    @Query('limit') limit: 100,
+    @Query('offset') offset: 50,
+    @Query('brand') brand: string,
+  ) {
+    return {
+      message: `products limit=>${limit} offset=>${offset} brand=>${brand}`,
+    };
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'accion de crear',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(
+    @Res() response: Response,
+    @Param('id') id: number,
+    @Body() payload: any,
+  ) {
+    response.status(203).send({
+      id,
+      message: 'accion de actualizar',
+      payload,
+    });
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NOT_MODIFIED)
+  delete(@Param('id') id: number) {
+    return {
+      id,
+      message: 'accion de borrar',
+    };
+  }
+}
